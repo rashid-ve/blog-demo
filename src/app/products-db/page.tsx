9a331c5d5500b4c5bc3826fd898fd36f96e664fa
rashid-ve <rashidashraf@teckvalley.com>
2025-01-27 20:02:10 +0530

    First commit

---
+++ src/app/products-db/page.tsx
@@ -0,0 +1,14 @@
+import { getProduct } from "../prisma-db";
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string | null;
+};
+
+export default async function ProductsDBPage() {
+  // const products: Product[] = await getProduct()
+
+  return <div></div>;
+}
